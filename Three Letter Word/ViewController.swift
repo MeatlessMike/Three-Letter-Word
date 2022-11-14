14967d08c56b969e71915d956b475dbf9cfe5041
Akivah <akivahlaviolette@gmail.com>
2022-11-14 09:45:51 -0700

    Initial Commit

---
+++ Three Letter Word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Three Letter Word
+//
+//  Created by Laviolette, Akivah - Student on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
